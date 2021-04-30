1be73a3b363f8874baf2b9ce2f7dc1fbabb7feb4
spoiler335 <54944126+spoiler335@users.noreply.github.com>
2021-04-30 10:57:09 +0530

    Selecting a Unit

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Mirror;
+
+public class Unit : NetworkBehaviour
+{
+    [SerializeField] private UnityEvent onSelected = null;
+    [SerializeField] private UnityEvent onDeSelected = null;
+
+    #region Client
+    public void Select()
+    {
+        if(!hasAuthority) { return; }
+
+        onSelected?.Invoke();
+    }
+
+    public void deSelect()
+    {
+        if (!hasAuthority) { return; }
+
+        onDeSelected?.Invoke();
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UnitSelectionHandler : MonoBehaviour
+{
+    [SerializeField] private LayerMask layerMask = new LayerMask();
+    private Camera mainCamera;
+
+    private List<Unit> selectedUnits = new List<Unit>();
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if(Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            foreach (Unit selectedUnit in selectedUnits)
+            {
+                selectedUnit.deSelect();
+            }
+
+            selectedUnits.Clear();
+        }
+
+        else if(Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            clearSelectionArea();
+        }
+    }
+
+    private void clearSelectionArea()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if(!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity,layerMask))
+        {
+            return; 
+        }
+
+        if(!hit.collider.TryGetComponent<Unit>(out Unit unit) )
+        { return; }
+
+        if(!unit.hasAuthority) { return; }
+
+        selectedUnits.Add(unit);
+
+        foreach(Unit selectedUnit in selectedUnits)
+        {
+            selectedUnit.Select();
+        }
+    }
+}
